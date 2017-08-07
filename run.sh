16b50c57db229a2e161feb40cfcfa739d2965679
Mika Majakorpi <mika@majakorpi.net>
2017-08-07 19:24:40 +0300

    Use base64 for cert and key env variables

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # lol
-echo "$DEVICE_AND_CA_CERT" > deviceCertAndCACert.pem
+echo $(echo "$DEVICE_AND_CA_CERT") | base64 -d > deviceCertAndCACert.pem
 export CERTIFICATE_PATH=deviceCertAndCACert.pem
-echo "$PRIVATE_KEY" > deviceCert.key
+echo $(echo "$PRIVATE_KEY") | base64 -d > deviceCert.key
 export PRIVATE_KEY_PATH=deviceCert.key
-echo "$AWS_ROOT_CA" > awsRootCA.pem
+echo $(echo "$AWS_ROOT_CA") | base64 -d > awsRootCA.pem
 export AWS_ROOT_CA_PATH=awsRootCA.pem
 
 python run.py
