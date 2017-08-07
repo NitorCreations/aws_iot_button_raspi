143c95ad89635a534c82072c3127e4952c9429a3
Mika Majakorpi <mika@majakorpi.net>
2017-08-07 16:30:04 +0300

    fix line feeds

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # lol
-echo $DEVICE_AND_CA_CERT > deviceCertAndCACert.pem
+echo "$DEVICE_AND_CA_CERT" > deviceCertAndCACert.pem
 export CERTIFICATE_PATH=deviceCertAndCACert.pem
-echo $PRIVATE_KEY > deviceCert.key
+echo "$PRIVATE_KEY" > deviceCert.key
 export PRIVATE_KEY_PATH=deviceCert.key
-echo $AWS_ROOT_CA > awsRootCA.pem
+echo "$AWS_ROOT_CA" > awsRootCA.pem
 export AWS_ROOT_CA_PATH=awsRootCA.pem
 
 python run.py
